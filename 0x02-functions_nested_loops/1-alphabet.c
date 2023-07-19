cb1fd6e876af803a6cd2faab5727659b51c69472
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-07-19 11:59:53 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make  the alphabet
+ *
+ * Return: always 0 when success
+ */
+
+void print_alphabet(void)
+
+{
+	char f;
+
+	for (f = 'a' ; f <= 'z' ; f++)
+		_putchar(f);
+	_putchar('\n');
+
+}
