a2ab473cf1fe6965879d50aab62dacdcf7310e16
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-07-20 14:44:58 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - the mul function
+ * @a: the function parameter
+ * @b: the function parameter
+ * Return: (a * b)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
