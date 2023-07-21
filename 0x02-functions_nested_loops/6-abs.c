760b2150de7f057933ae2d55d43c3d28d4530b5b
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-07-21 13:44:43 -0700

    main file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - value of a number
+ * @n: the function parameter
+ * Return: -n or n;
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else if (n >= 0)
+	{
+		return (n);
+	}
+}
