fa48a00698eba879b6e53efa3ce08633d7f5a127
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-08-04 10:32:30 -0700

    my new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the argument parameter
+ * @argv: the argument parameter
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
