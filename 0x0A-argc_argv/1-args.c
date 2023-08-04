3bb75aff5855cfce254df519032b9fefdeaea215
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-08-04 10:55:30 -0700

    hello

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the argument c function
+ * @argv: the argument v function
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
