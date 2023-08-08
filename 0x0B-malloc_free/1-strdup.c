38606ea56b30ad79767bb9e07a0989d1b101bcb7
Subaia Ibraheem <subaiaibraheem@gmail.com>
2023-08-08 14:09:54 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strdup - the string main function
+ * @str: the string parameter
+ * Return: the pointer to the memory
+ */
+
+char *_strdup(char *str)
+{
+	char *copy;
+	int count = 0;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		count++;
+
+	copy = malloc(sizeof(char) * count + 1);
+
+	if (copy == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		copy[i] = str[i];
+
+	return (copy);
+}
